0cb11b16900b6416c817bef692a951663ac5af94
camarguinho <linkcamargo@gmail.com>
2026-02-07 17:41:42 -0300

    diferentes estrategias de fallback

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.function.Supplier;
 
+/**
+ * Regras de ouro antes de começar
+ * Optional é para retornos de métodos.
+ * Não use em atributos, parâmetros ou coleções.
+ *
+ * Optional não é para tudo.
+ * É para quando a ausência de valor faz parte do domínio.
+ *
+ * Optional é para fluência funcional.
+ * Use map, flatMap, filter, orElseGet, etc.
+ *
+ * Optional não substitui validação.
+ * Ele só modela ausência, não garante consistência.
+ */
 public class Main {
 
     public static void main(String[] args){
@@ -13,7 +29,20 @@ public class Main {
         System.out.println(length.orElse(0));*/
 
         //transformValueMap();
-        transformValueFlatMap();
+        //transformValueFlatMap();
+        //filterValue();
+        //fallBackEager();
+        fallBackLazy();
+    }
+
+    private static Optional<User> findUser(){
+        return Optional.of(new User("Camargo", Optional.of("linkcamargo@gmail.com")));
+        //return Optional.empty();
+    }
+
+    private static Supplier<String> findDefaultValue() {
+        Supplier<String> sup = () -> "default";
+        return sup;
     }
 
     /**
@@ -47,7 +76,51 @@ public class Main {
         System.out.println(email.get());
     }
 
-    private static Optional<User> findUser(){
-       return Optional.of(new User("Camargo", Optional.of("linkcamargo@gmail.com")));
+    /**
+     * Mantém o valor apenas se passar no predicado.
+     * filter(Predicate<T>)
+     *
+     * Caso de uso:
+     * -Validações simples
+     * -Regras de negócio booleanas
+     */
+    private static void filterValue(){
+        Optional<User> user = findUser();
+        Optional<User> startWC = user.filter(u -> u.name().startsWith("C"));
+        System.out.println(startWC);
     }
+
+    /**
+     * orElse(value)
+     * Retorna o valor padrão sempre, mesmo que o Optional esteja cheio.
+     *
+     * Cuidado:
+     * Se o valor padrão for caro de calcular, ele será calculado mesmo sem necessidade.
+     */
+    private static void fallBackEager(){
+        Optional<User> user = findUser();
+        Optional<String> email = user.flatMap(User::email);
+        System.out.println(email.orElse("xiii"));
+    }
+
+    /**
+     * orElseGet(Supplier<T>)
+     * Retorna o valor padrão preguiçosamente.
+     *
+     * Caso de uso:
+     * -Valores caros de calcular
+     * -Acesso a banco
+     * -Chamadas externas
+     *
+     * “orElseGet só executa o Supplier se o Optional estiver vazio.”
+     */
+    private static void fallBackLazy(){
+        Optional<User> user = findUser();
+        Optional<String> email = user.flatMap(User::email);
+        System.out.println(email.orElseGet(findDefaultValue()));
+    }
+
+
+
+
 }
